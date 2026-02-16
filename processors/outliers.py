22a7e230a510f0c17b5897262adb5c69de977ae2
MamadouSaidouKeita <keitamamadousaidou076@gmail.com>
2026-02-16 18:48:05 +0000

    modif va

--- processors/outliers.py
+++ processors/outliers.py
@@ -12,7 +12,10 @@ def handle_outliers_smart(df):
         lower = Q1 - 1.5 * IQR
         upper = Q3 + 1.5 * IQR
 
-        # Remplacer les valeurs hors bornes par lower ou upper
-        df_res[col] = np.clip(df_res[col], lower, upper)
+        df_res[col] = df_res[col].apply(
+            lambda x: lower if x < lower
+            else upper if x > upper
+            else x
+        )
 
     return df_res
